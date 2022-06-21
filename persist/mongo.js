2fe2379726239b65c1d1f3c093b3084f08647f4a
Cody Lehnhausen <codylehnhausen@Codys-MacBook-Pro.local>
2022-06-21 12:35:38 -0600

    Mongo

--- persist/mongo.js
+++ persist/mongo.js
@@ -3,9 +3,35 @@ const mongoose = require('mongoose');
 // rename mongoose connection
 const db = mongoose.connection;
 
-const connectionString = `mongodb://{};{}@{host}:{port}/{database}`;
+function connect(user, password, host, port, db) {
+  const connectionString = `mongodb+srv://user:password@cluster0.kygdq0t.mongodb.net/?retryWrites=true&w=majority`;
 
-mongoose.connect(connectionString, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+  mongoose.connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+}
+//what should happen when start connecting
+function setUpConnectionHandlers(callback) {
+  db.once('connecting', () => {
+    console.log('connecting to MongoDB');
+  });
+  //what should happen when connected
+  db.once('connected', () => {
+    console.log('connected to MongoDB');
+  });
+  //what should happen when opens
+  db.once('open', () => {
+    console.log('open connection to MongoDB');
+    callback();
+  });
+  //what should happen when error
+  db.once('error', () => {
+    console.log('Error connecting to MongoDB');
+  });
+}
+//export the functions
+module.exports = {
+  connect: connect,
+  setUpConnectionHandlers,
+};
